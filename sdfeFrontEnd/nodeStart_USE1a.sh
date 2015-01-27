e6c502ff9599299e6b080cbc9f5a7ddcf0dbecf7
priyajs <priya.jayaraman@elsevier.com>
2015-01-27 13:22:25 -0500

    ELB for USE1

--- sdfeFrontEnd/nodeStart_USE1a.sh
+++ sdfeFrontEnd/nodeStart_USE1a.sh
@@ -6,3 +6,4 @@ sudo pkill java
 
 
 sudo xvfb-run --server-args='+extension RANDR -screen 0 1280x800x24' java -jar /home/ubuntu/selenium-server.jar -role node -browser browserName=chrome,maxInstances=6 -maxSession 6 -port 4320 -hub http://sdfe-se-748397699.us-east-1.elb.amazonaws.com/grid/register -registerCycle 5000 &
+
