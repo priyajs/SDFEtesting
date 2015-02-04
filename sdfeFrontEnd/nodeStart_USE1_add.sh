c2584e8416c68382e02bb5b521b4bfb83f887f99
priyajs <priyajs@users.noreply.github.com>
2015-02-04 11:38:04 -0500

    updating with sdfe-se-add ELB

--- sdfeFrontEnd/nodeStart_USE1_add.sh
+++ sdfeFrontEnd/nodeStart_USE1_add.sh
@@ -5,5 +5,5 @@ sudo pkill chrome
 sudo pkill java
 
 
-sudo xvfb-run --server-args='+extension RANDR -screen 0 1280x800x24' java -jar /home/ubuntu/selenium-server.jar -role node -browser browserName=chrome,maxInstances=6 -maxSession 6 -port 4320 -hub http://sdfe-se-748397699.us-east-1.elb.amazonaws.com/grid/register -registerCycle 5000 &
+sudo xvfb-run --server-args='+extension RANDR -screen 0 1280x800x24' java -jar /home/ubuntu/selenium-server.jar -role node -browser browserName=chrome,maxInstances=6 -maxSession 6 -port 4320 -hub http://sdfe-se-add-1412103930.us-east-1.elb.amazonaws.com/grid/register -registerCycle 5000 &
 
