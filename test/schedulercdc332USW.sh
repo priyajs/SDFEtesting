d5a6f129d7dd072bde6988b1324d28f036a87d8c
priyajs <priya.jayaraman@elsevier.com>
2015-03-04 12:32:13 -0500

    adding sleep time before deleting stac

--- test/schedulercdc332USW.sh
+++ test/schedulercdc332USW.sh
@@ -21,6 +21,7 @@ sleep 10m
 #
 echo "`date` creating new stack cdc332soaktest-VPC us-west-2" >> $LOGFILE
 aws cloudformation create-stack --stack-name cdc332soaktest-VPC --template-url https://s3-us-west-2.amazonaws.com/sdfe-testresources/test/SDFE-SeGrid-20141222-VPC-usw2-cdc332.template  --parameters ParameterKey=MaxASGSize,ParameterValue=1 --capabilities CAPABILITY_IAM --region us-west-2
+sleep 10m
 # delete the existing stack in ap-southeast-1
 #
 echo "`date` deleting CF stack cdc332soaktest ap-southeast-1" >> $LOGFILE
@@ -38,6 +39,7 @@ echo "`date` creating new stack cdc332soaktest ap-southeast-1" >> $LOGFILE
 aws cloudformation create-stack --stack-name cdc332soaktest --template-url https://s3-us-west-2.amazonaws.com/sdfe-testresources/sdfeFrontEnd/SDFE-SeGrid-20141022-se1-cdc332.template  --parameters ParameterKey=MaxASGSize,ParameterValue=1 --capabilities CAPABILITY_IAM --region ap-southeast-1
 # delete the existing stack in eu-west-1
 #
+sleep 10m
 echo "`date` deleting CF stack cdc332soaktest eu-west-1" >> $LOGFILE
 aws cloudformation delete-stack --stack-name cdc332soaktest --region eu-west-1
 #
