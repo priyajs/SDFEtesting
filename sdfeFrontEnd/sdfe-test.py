1a7a79ebec3df76fd14f57d1612defabc18999c7
priyajs <priya.jayaraman@elsevier.com>
2015-03-16 17:02:54 -0400

    changes for 1.1 test files

--- sdfeFrontEnd/sdfe-test.py
+++ sdfeFrontEnd/sdfe-test.py
@@ -54,11 +54,18 @@ addr=(statsDHost,8125)
 PII=[]
 try:
   #csvRd = csv.reader(open('/home/ubuntu/PIIs_250k.csv','rb'))
-  csvRd = csv.reader(open('/home/ubuntu/sdfePIIwhitelistwl.csv','rb'))
-  piiCount = 4300
+  if (region == 'USE1' or region == 'USW2'):
+	csvRd = csv.reader(open('/home/ubuntu/sdfePIIwhitelist1.1_1.csv','rb'))
+  elif (region == 'EUW1'):
+    csvRd = csv.reader(open('/home/ubuntu/sdfePIIwhitelist1.1_2.csv','rb'))
+  elif (region == 'APSE1'):
+    csvRd = csv.reader(open('/home/ubuntu/sdfePIIwhitelist1.1_3.csv','rb'))
+  else:
+	csvRd = csv.reader(open('/home/ubuntu/sdfePIIwhitelist1.1_1.csv','rb'))
+  piiCount = 30000
 except:
-  csvRd = csv.reader(open('C:/Scripts/sdfePIIwhitelistwl.csv','rb'))
-  piiCount = 4300
+  csvRd = csv.reader(open('C:/Scripts/sdfePIIwhitelist1.1_1.csv','rb'))
+  piiCount = 30000
 for j in csvRd:
   PII.append(j)
 
